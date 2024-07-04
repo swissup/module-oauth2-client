fe88b6d421785a07c5f32a932a6e6b5a234291fd
Alexander Kras'ko <0m3r.mail@gmail.com>
2024-07-04 17:38:28 +0300

    Rename FlowToken => BearerToken

--- Api/BearerTokenInterface.php
+++ Api/BearerTokenInterface.php
@@ -1,11 +1,13 @@
 <?php
 namespace Swissup\OAuth2Client\Api;
 
-interface FlowTokenInterface
+interface BearerTokenInterface
 {
     public function getToken();
 
     public function validate($token);
+
+    public function getParamName():string;
 
     public function validateRequest(\Magento\Framework\App\RequestInterface $request);
 }
--- Model/Data/BearerToken.php
+++ Model/Data/BearerToken.php
@@ -3,10 +3,11 @@
 namespace Swissup\OAuth2Client\Model\Data;
 
 use Magento\Framework\Encryption\Helper\Security;
+use Magento\Framework\Exception\LocalizedException;
 
-class FlowToken implements \Swissup\OAuth2Client\Api\FlowTokenInterface
+class BearerToken implements \Swissup\OAuth2Client\Api\BearerTokenInterface
 {
-    const CACHE_ID = 'swissup_oauth2_client_flow_1h_token';
+    const CACHE_ID = 'swissup_oauth2_client_1h_bearer_token';
     const CACHE_TAG = 'swissup_oauth2_client';
     const LIFETIME = 3600; // 1 hour
 
@@ -18,7 +19,7 @@ class FlowToken implements \Swissup\OAuth2Client\Api\FlowTokenInterface
     /**
      * @var \Magento\Framework\Cache\FrontendInterface
      */
-    private $cache;
+    private $cacheFrontend;
 
     /**
      * @param \Magento\Framework\Math\Random $mathRandom
@@ -26,10 +27,10 @@ class FlowToken implements \Swissup\OAuth2Client\Api\FlowTokenInterface
      */
     public function __construct(
         \Magento\Framework\Math\Random $mathRandom,
-        \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontend
+        \Magento\Framework\App\Cache\Frontend\Pool $cacheFrontendPool
     ) {
         $this->mathRandom = $mathRandom;
-        $this->cache = $cacheFrontend->get(\Magento\Framework\App\Cache\Frontend\Pool::DEFAULT_FRONTEND_ID);
+        $this->cacheFrontend = $cacheFrontendPool->get(\Magento\Framework\App\Cache\Frontend\Pool::DEFAULT_FRONTEND_ID);
     }
 
     /**
@@ -43,7 +44,7 @@ class FlowToken implements \Swissup\OAuth2Client\Api\FlowTokenInterface
         if (!$this->isPresent()) {
             $this->set($this->mathRandom->getRandomString(16));
         }
-        return $this->cache->load(self::CACHE_ID);
+        return $this->cacheFrontend->load(self::CACHE_ID);
     }
 
     /**
@@ -53,7 +54,7 @@ class FlowToken implements \Swissup\OAuth2Client\Api\FlowTokenInterface
      */
     private function isPresent()
     {
-        return (bool) $this->cache->test(self::CACHE_ID);
+        return (bool) $this->cacheFrontend->test(self::CACHE_ID);
     }
 
     /**
@@ -64,18 +65,28 @@ class FlowToken implements \Swissup\OAuth2Client\Api\FlowTokenInterface
      */
     private function set($value)
     {
-        $this->cache->save((string) $value, self::CACHE_ID, [self::CACHE_TAG], self::LIFETIME);
+        $this->cacheFrontend->save((string) $value, self::CACHE_ID, [self::CACHE_TAG], self::LIFETIME);
     }
+
 
     /**
      * Validate
      *
      * @param \Magento\Framework\App\RequestInterface $request
      * @return bool
+     * @throws LocalizedException
      */
     public function validate($token)
     {
         return $token && Security::compareStrings($token, $this->getToken());
+    }
+
+    /**
+     * @return string
+     */
+    public function getParamName():string
+    {
+        return 'bearer';
     }
 
     /**
@@ -86,7 +97,7 @@ class FlowToken implements \Swissup\OAuth2Client\Api\FlowTokenInterface
      */
     public function validateRequest(\Magento\Framework\App\RequestInterface $request)
     {
-        $token = $request->getParam('token', null);
+        $token = $request->getParam($this->getParamName(), null);
         return $this->validate($token);
     }
 }
